81c7c4bd9f8eec6dfa0318eae6a8395a3959c284
prathikshakamathk <prathiksha.kamath@niveussolutions.com>
2023-03-11 06:36:34 +0000

    Added multiple tag change 3

--- variables.tf
+++ variables.tf
@@ -16,12 +16,12 @@ variable "region" {
 
 variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
-  default     = "10.0.0.0/16"
+  default     = "172.16.0.0/16"
 }
 
 variable "subnet_prefix" {
   description = "The address prefix to use for the subnet."
-  default     = "10.0.10.0/24"
+  default     = "172.16.10.0/24"
 }
 
 variable "instance_type" {
